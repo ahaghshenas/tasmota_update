4bdf4ca9d4e86271023e8aa2130ee321ff6d709b
root <root@f5588468-ha-opencode.local.hass.io>
2026-07-26 00:58:16 +0400

    v0.8.4: Fix options flow using OptionsFlowWithConfigEntry

--- custom_components/tasmota_update/config_flow.py
+++ custom_components/tasmota_update/config_flow.py
@@ -58,12 +58,8 @@ class TasmotaUpdateConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return TasmotaUpdateOptionsFlow(config_entry)
 
 
-class TasmotaUpdateOptionsFlow(config_entries.OptionsFlow):
+class TasmotaUpdateOptionsFlow(config_entries.OptionsFlowWithConfigEntry):
     """Handle options flow for Tasmota Update."""
-
-    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
-        """Initialize options flow."""
-        self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
